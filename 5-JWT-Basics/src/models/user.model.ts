1b37b97c13cc8ec8596f4c8d2e8ed88ac10e7a78
MiniGT <bendanillo539@gmail.com>
2024-12-06 19:26:21 +0800

    feat(server): setting up user schema model

---
+++ 5-JWT-Basics/src/models/user.model.ts
@@ -0,0 +1,14 @@
+import mongoose, { InferSchemaType } from "mongoose"
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+})
+
+type User = InferSchemaType<typeof userSchema>
+
+export default mongoose.model<User>("Note", userSchema)
