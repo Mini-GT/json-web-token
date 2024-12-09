3977bac47c036f1f6de467478063bd00feb9cf72
MiniGT <bendanillo539@gmail.com>
2024-12-09 20:45:43 +0800

    feat(server): user controller

---
+++ 5-JWT-Basics/src/controllers/users.controller.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from "express"
+
+const getUsers = async (req: Request, res: Response)=> {
+    res.json({msg: "testing"})
+}
+
+const createUser = async (req: Request, res: Response) => {
+    const {name} = req.params
+    console.log(name)
+}
+
+export {
+    getUsers,
+    createUser,
+}
