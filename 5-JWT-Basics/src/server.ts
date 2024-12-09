a14e3dd64698c04999e0d2cb05ee44ac98d55af8
MiniGT <bendanillo539@gmail.com>
2024-12-09 20:44:40 +0800

    feat(server): added routes

--- 5-JWT-Basics/src/server.ts
+++ 5-JWT-Basics/src/server.ts
@@ -2,6 +2,9 @@ import express from "express"
 import "dotenv/config"
 import errorHandlerMiddleware from "./middlewares/errorHandler.middleware"
 import notFound from "./middlewares/notFound.middleware"
+import userRouter from "./routes/login.route"
+import mainRouter from "./routes/main.route"
+import { connectDB } from "../db/connect"
 
 const app = express()
 
@@ -11,9 +14,13 @@ const port = process.env.PORT
 app.use(express.static('./public'))
 app.use(express.json())
 
+app.use('/api/v1/users', userRouter)
+app.use('/api/v1', mainRouter)
+
 app.use(errorHandlerMiddleware)
 app.use(notFound)
 
 app.listen(port, () => {
-    console.log(`Running on Port ${port}...`)
+  // connectDB()
+  console.log(`Running on Port ${port}...`)
 })
