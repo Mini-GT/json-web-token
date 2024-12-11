ca1e2b748b9ab2044714ca7af117c5b08edd1c4f
MiniGT <bendanillo539@gmail.com>
2024-12-11 20:50:57 +0800

    created user controller

--- 5-JWT-Basics/src/controllers/users.controller.ts
+++ 5-JWT-Basics/src/controllers/users.controller.ts
@@ -1,15 +1,15 @@
 import { Request, Response } from "express"
 
 const getUsers = async (req: Request, res: Response)=> {
-    res.json({msg: "testing"})
+  res.json({msg: "testing"})
 }
 
 const createUser = async (req: Request, res: Response) => {
-    const {name} = req.params
-    console.log(name)
+  const {name} = req.params
+  console.log(name)
 }
 
 export {
-    getUsers,
-    createUser,
+  getUsers,
+  createUser,
 }
