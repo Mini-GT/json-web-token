a94c5a02f14b45f596c42802250c256217ebb267
MiniGT <bendanillo539@gmail.com>
2024-12-11 20:25:32 +0800

    authentication middleware

--- 5-JWT-Basics/src/routes/main.route.ts
+++ 5-JWT-Basics/src/routes/main.route.ts
@@ -1,12 +1,13 @@
 import express from "express"
 import { asyncWrapper } from "../middlewares/asyncWrapper"
 import { login, dashboard } from "../controllers/main.controller"
+import { authentication } from "../middlewares/auth.middleware"
 
 const router = express.Router()
 
 //routes
 router.route("/dashboard")
-.get(asyncWrapper(dashboard))
+.get(authentication, asyncWrapper(dashboard))
 
 router.route("/login")
 .post(asyncWrapper(login))
